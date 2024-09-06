dd666c80967457a3256b245b4c0108cc10b9b858
jwcomp4 <jwcompton4@gmail.com>
2024-09-05 22:32:42 -0400

    format home.py

--- pages/home.py
+++ pages/home.py
@@ -1,13 +1,14 @@
-import dash 
+import dash
 from dash import dcc, html, callback, Input, Output, State
 import pandas as pd
 import dash_mantine_components as dmc
 import plotly.express as px
 from utils.data_utils import tree_clean
 
-dash.register_page(__name__, path='/')
+dash.register_page(__name__, path="/")
 
 tree = pd.read_csv("tree.csv")
+
 
 def layout():
     layout = dmc.Container(
@@ -19,28 +20,30 @@ def layout():
                         label="Planted Trees or Planting Sites?",
                         id="site-or-tree",
                         data=[
-                            {"value":"Tree", "label":"Tree"},
-                            {"value":"Planting Site", "label":"Planting Site"},
+                            {"value": "Tree", "label": "Tree"},
+                            {"value": "Planting Site", "label": "Planting Site"},
                         ],
-                        
-                    ), 
-                    dmc.Container(
-                        [
-                            dcc.Graph(id='tree-map')
-                        ]
-                        )
-                ], align='center', justify='flex-start', gap={"base":"sm"}
+                    ),
+                    dmc.Container([dcc.Graph(id="tree-map")]),
+                ],
+                align="center",
+                justify="flex-start",
+                gap={"base": "sm"},
             )
         ]
     )
     return layout
 
-@callback(
-    Output('tree-map', 'figure'),
-    Input('site-or-tree', 'value')
-)
+
+@callback(Output("tree-map", "figure"), Input("site-or-tree", "value"))
 def map_update(value):
     df = tree_clean(value, tree)
-    fig = px.scatter_map(df, lat="Y", lon="X", map_style='carto-positron',
-                             hover_data=['genus','species', 'commonname', 'diameterin', 'plantingdate'], zoom=10)
+    fig = px.scatter_map(
+        df,
+        lat="Y",
+        lon="X",
+        map_style="carto-positron",
+        hover_data=["genus", "species", "commonname", "diameterin", "plantingdate"],
+        zoom=10,
+    )
     return fig
