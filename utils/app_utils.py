177bd7efcbecef4fb3281e8ff2d258200167417c
jwcomp4 <jwcompton4@gmail.com>
2024-11-01 22:38:24 -0400

    Capture click data, isolating desired tree data, create card function to display, start callback dev.

--- utils/app_utils.py
+++ utils/app_utils.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 import requests
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
 
 # Loading in environment variable
 
@@ -8,12 +10,29 @@ load_dotenv()
 
 GOOGLE_MAP_KEY = os.environ.get("GOOGLE_MAP_KEY")
 
+
 def geocode_address(address):
     key = GOOGLE_MAP_KEY
     url_base = "https://maps.googleapis.com/maps/api/geocode/json?address="
     geocode_url = f"{url_base}{address}&key={key}"
-    response = requests.get(geocode_url).json()['results'][0]['geometry']['location']
-    lat = response['lat']
-    lon = response['lng']
+    response = requests.get(geocode_url).json()["results"][0]["geometry"]["location"]
+    lat = response["lat"]
+    lon = response["lng"]
     return lat, lon
-    
+
+
+def make_card(icon, attribute):
+    card = dmc.Card(
+        [dmc.Group([DashIconify(icon=icon, width=30), dmc.Text(attribute)])],
+        withBorder=True,
+        shadow="sm",
+        radius="md",
+    )
+    return card
+
+card_icons = {
+    "tree": "lucide:trees",
+    "diameter": "game-icons:measure-tape",
+    "date": "fluent:calendar-24-regular"
+}
+
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -5,3 +5,8 @@
 def tree_clean(exist,data):
     df = data.loc[data['present']==exist,["X","Y","genus", "species", "commonname", "coremoved_ozperyr", "carbonstorage_lb", 'diameterin','plantingdate']].fillna("not available")
     return df
+
+def click_tree_clean(exist,data, latitude, longitude):
+    df = data.loc[data['present']==exist,["X","Y","genus", "species", "commonname", "coremoved_ozperyr", "carbonstorage_lb", 'diameterin','plantingdate']].fillna("not available")
+    df = df[(df['Y']==latitude) & (df['X']==longitude)]
+    return df
